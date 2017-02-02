cc306380cf2a2e0cf987dd54f7af46c133f90c87
Jonathan Prieto-Cubides <prieto.jona@gmail.com>
2017-02-02 10:07:00 -0500

    added tptp2X utility

--- deveagda.sh
+++ deveagda.sh
@@ -3,6 +3,15 @@ cd eagda
 git pull origin master
 make install-bin
 cd $HOME
+
+# --------------------------------------
+# tptp2X Utility
+#cd $HOME/TPTP-v6.4.0/Scripts/
+#csh ./tptp2T_install
+#cd $HOME/TPTP-v6.4.0/TPTP2X/
+#csh ./tptp2X_install 
+
+# --------------------------------------
 cd apia
 git pull origin master
 make install-bin
