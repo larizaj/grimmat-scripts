b4acd7a8657fe115e2d01edba5559592e2f9e8b9
alejogm0520 <agomez13@eafit.edu.co>
2017-02-03 15:28:14 -0500

    Create NetCDFInstall.sh

---
+++ NetCDFInstall.sh
@@ -0,0 +1,27 @@
+sudo dnf install hdf5* -y
+sudo dnf install gcc* -y
+sudo dnf install openmp* -y
+sudo dnf install mpi* -y
+
+
+export FC=gfortran
+export F77=gfortran
+export CC=gcc
+export CXX=g++
+
+wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.4.1.1.tar.gz
+
+tar -xvf netcdf-4.4.1.1.tar.gz 
+
+rm netcdf-4.4.1.1.tar.gz 
+cd netcdf-4.4.1.1/
+sudo mkdir home/${USER}/opt/netcdf
+sudo mkdir home/${USER}/opt/netcdf/4.4.1.1
+
+./configure --prefix=home/${USER}/opt/netcdf/4.4.1.1 
+
+make
+
+sudo make install
+make check
+
