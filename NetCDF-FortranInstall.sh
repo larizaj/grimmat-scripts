8234a293dade5bba84d327696aff4826e16a20bd
alejogm0520 <agomez13@eafit.edu.co>
2017-02-03 20:11:59 -0500

    Create NetCDF-FortranInstall.sh

---
+++ NetCDF-FortranInstall.sh
@@ -0,0 +1,11 @@
+mkdir /home/${USER}/opt/netcdf/4.4.1.1/netcdf-fortran
+mkdir /home/${USER}/opt/netcdf/4.4.1.1/netcdf-fortran/4.4.4
+
+wget ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-fortran-4.4.4.tar.gz
+tar -xvf netcdf-fortran-4.4.4.tar.gz 
+rm netcdf-fortran-4.4.4.tar.gz 
+cd netcdf-fortran-4.4.4
+nethome=/home/${USER}/opt/netcdf/4.4.1.1
+sudo chmod u+rx configure
+sudo chmod +x configure
+./configure --prefix=${nethome}/netcdf-fortran/4.4.4 CPPFLAGS="-I${nethome}/include" LDFLAGS="-L${nethome}/lib" CC='gcc' CXX='g++' F77='gfortran' FC='gfortran' --disable-netcdf-4
