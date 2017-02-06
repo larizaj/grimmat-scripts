c4d7aaf339396555d78e604ed354068e7b30d1a4
alejogm0520 <agomez13@eafit.edu.co>
2017-02-06 17:26:52 -0500

    Update NetCDF-FortranInstall.sh

--- NetCDF-FortranInstall.sh
+++ NetCDF-FortranInstall.sh
@@ -6,6 +6,7 @@ tar -xvf netcdf-fortran-4.4.4.tar.gz
 rm netcdf-fortran-4.4.4.tar.gz 
 cd netcdf-fortran-4.4.4
 nethome=/home/${USER}/opt/netcdf/4.4.1.1
+export LD_LIBRARY_PATH=${nethome}/lib
 sudo chmod u+rx configure
 sudo chmod +x configure
 ./configure --prefix=${nethome}/netcdf-fortran/4.4.4 CPPFLAGS="-I${nethome}/include" LDFLAGS="-L${nethome}/lib" CC='gcc' CXX='g++' F77='gfortran' FC='gfortran' --disable-netcdf-4
