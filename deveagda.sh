5c606b72a72c2015f60f2eba45a2388376c8928a
Jonathan Prieto-Cubides <prieto.jona@gmail.com>
2017-02-06 09:46:37 -0500

    Update deveagda.sh

--- deveagda.sh
+++ deveagda.sh
@@ -1,22 +1,22 @@
-cd $HOME
+cd ${HOME}
 cd eagda
 git pull origin master
 make install-bin
-cd $HOME
+cd ${HOME}
 
 # --------------------------------------
 # tptp2X Utility
-#cd $HOME/TPTP-v6.4.0/Scripts/
+#cd ${HOME}/TPTP-v6.4.0/Scripts/
 #csh ./tptp2T_install
-#cd $HOME/TPTP-v6.4.0/TPTP2X/
-#csh ./tptp2X_install 
+#cd ${HOME}/TPTP-v6.4.0/TPTP2X/
+#csh ./tptp2X_install
 
 # --------------------------------------
 cd apia
 git pull origin master
 make install-bin
 make tests
-cd $HOME
+cd ${HOME}
 cd online-atps
 git pull origin master
 make install-bin
