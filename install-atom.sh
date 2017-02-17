d64d366ada68b8248b51ae4d74b3c90d823548f5
Jonathan Prieto-Cubides <prieto.jona@gmail.com>
2017-02-17 09:43:12 -0500

    changed the default name rpm by atom-rpm

--- install-atom.sh
+++ install-atom.sh
@@ -1,6 +1,7 @@
-wget https://atom.io/download/rpm
-rpm --checksig rpm
+wget -o atom-rpm https://atom.io/download/rpm
+rpm --checksig atom-rpm
 sudo dnf install lsb-core-noarch -y
-sudo rpm -ivh rpm
-rpm -q rpm
+sudo rpm -ivh atom-rpm
+rpm -q atom-rpm
+rm -f atom-rpm
 # $ atom
