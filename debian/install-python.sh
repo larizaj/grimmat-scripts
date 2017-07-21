258d5128454c08b629cfd2edbff3556d07cbb6a5
Alejandro Gómez Montoya <agomez13@eafit.edu.co>
2017-07-21 11:19:51 -0500

    Create install-python.sh

---
+++ debian/install-python.sh
@@ -0,0 +1,12 @@
+sudo -i
+# su
+
+apt-get install python python-dev python3 python3-dev -y
+apt-get install python-numpy* python3-numpy* -y
+apt-get install python-scipy* python3-scipy* -y
+apt-get install python-pandas* python3-pandas* -y
+apt-get install python-matplolib* python3-matplolib* -y
+apt-get install python-scikits-learn python3-scikits-learn -y
+apt-get install python-pip python3-pip -y
+
+apt-get install spyder spyder3 -y
