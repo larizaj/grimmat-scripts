62a3980416f6affd3b3b52b42172150ec0ad82fc
Leandro Ariza <ariza.leandro@gmail.com>
2017-07-23 23:56:11 -0500

    Update install-mendeley.sh

--- debian/install-mendeley.sh
+++ debian/install-mendeley.sh
@@ -3,3 +3,9 @@ cd ~
 wget https://www.mendeley.com/repositories/ubuntu/stable/amd64/mendeleydesktop-latest
 
 sudo dpkg -i mendeleydesktop-latest
+
+# Although memdeleydesktop package has a source with an http:// address,
+# it will try connecting to https://.
+# To fix this potential error when trying "apt-get update" after package
+# installation:
+apt-get install apt-transport-https
